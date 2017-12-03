7e86deef6805bab07875a2f990797775552569bf
VonAerich <von.aerich@gmail.com>
2017-12-03 20:45:12 +0800

    Added save request base

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Utilities
 This project contains nuget packages for common utilities used across all projects.
+
+## To push
+1. Get key from myGet
+2. Update package version to something higher
+3. Use this command
+```
+dotnet nuget push SamplePackage.1.0.0.nupkg <your access token> --source https://www.myget.org/F/bluenebula-utilities/api/v3/index.json
+```
---
+++ src/BlueNebula.Common/DTOs/SaveRequestBase.cs
@@ -0,0 +1,35 @@
+﻿namespace BlueNebula.Common.DTOs
+{
+    using System;
+
+    /// <summary>
+    /// The base class for save requests.
+    /// </summary>
+    public class SaveRequestBase
+    {
+        /// <summary>
+        /// Gets or sets the id property.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the created by property.
+        /// </summary>
+        public int CreatedBy { get; set; }
+
+        /// <summary>
+        /// Gets or sets the created date property.
+        /// </summary>
+        public DateTime CreatedDate { get; set; }
+
+        /// <summary>
+        /// Gets or sets the last updated by property.
+        /// </summary>
+        public int LastUpdatedBy { get; set; }
+
+        /// <summary>
+        /// Gets or sets the last updated date property.
+        /// </summary>
+        public DateTime LastUpdatedDate { get; set; }
+    }
+}
