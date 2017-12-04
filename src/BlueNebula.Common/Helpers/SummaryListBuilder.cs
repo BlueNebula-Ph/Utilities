b1b940b067345ba4469a0b58a41b7060fea171d6
VonAerich <von.aerich@gmail.com>
2017-12-04 19:48:57 +0800

    Fixed the sorting issue.

--- src/BlueNebula.Common/Helpers/SummaryListBuilder.cs
+++ src/BlueNebula.Common/Helpers/SummaryListBuilder.cs
@@ -1,12 +1,13 @@
 ﻿namespace BlueNebula.Common.Helpers
 {
     using System;
+    using System.Collections.Generic;
     using System.Linq;
     using System.Linq.Dynamic.Core;
     using System.Threading.Tasks;
-    using AutoMapper.QueryableExtensions;
-    using Microsoft.EntityFrameworkCore;
     using BlueNebula.Common.DTOs;
+    using Microsoft.EntityFrameworkCore;
+    using AutoMapper.QueryableExtensions;
 
     /// <summary>
     /// The summary list builder
@@ -35,17 +36,32 @@
             var pageNumber = (filter?.PageIndex).IsNullOrZero() ? DEFAULT_PAGE_INDEX : filter.PageIndex;
             var pageSize = (filter?.PageSize).IsNullOrZero() ? DEFAULT_PAGE_SIZE : filter.PageSize;
 
+            // compute the totals
             var totalRecords = await source.CountAsync();
             var totalPages = (int)Math.Ceiling(totalRecords / (double)pageSize);
+
+            // prepare sorting
+            var ordering = $"Id desc";
+            if (!string.IsNullOrEmpty(filter?.SortBy))
+            {
+                ordering = $"{filter.SortBy} {filter.SortDirection}";
+            }
+
+            var itemsToSkip = pageSize * (pageNumber - 1);
+
             var items = source
-                .Page(pageNumber, pageSize)
+                .OrderBy(ordering)
                 .ProjectTo<T1>();
+
+            var pagedItems = items
+                .Page(pageNumber, pageSize)
+                .ToList();
 
             return new SummaryList<T1>()
             {
                 TotalPages = totalPages,
                 PageIndex = pageNumber,
-                Items = items,
+                Items = pagedItems,
             };
         }
     }
